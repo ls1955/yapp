8807722a53dbdebe345e8cba96e48e71453ac9b6
ls1955 <xxlsxx1955@gmail.com>
2024-04-15 19:14:32 +0800

    Include navigation example

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def goodbye_world():
-    return "<p>Goodbye, world.</p>"
+def index():
+    return render_template("index.html", tuna_endpoint="tuna")
+
+@app.route("/tuna")
+def tuna():
+    return render_template("tuna.html", home_endpoint="/")
