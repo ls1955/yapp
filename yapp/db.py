0641e71b6463642dc8fe25a5097a20829909f73c
ls1955 <xxlsxx1955@gmail.com>
2024-04-15 20:54:35 +0800

    Fix wrong method name
    ...it wasn't snake_case.

--- yapp/db.py
+++ yapp/db.py
@@ -18,7 +18,7 @@ def init_db():
     db = get_db()
 
     with current_app.open_resource("schema.sql") as f:
-        db.execute_script(f.read().decode("utf8"))
+        db.executescript(f.read().decode("utf8"))
 
 
 @click.command("init-db")
