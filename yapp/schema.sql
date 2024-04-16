3356464a824e8580a590f68ccdf41fb11e8feba6
Cheong <xxlsxx1955@gmail.com>
2024-04-16 08:28:59 +0800

    Modify users table
    This commit:
    * Add `name` column
    * Add `encrypted_password` column

--- yapp/schema.sql
+++ yapp/schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    encrypted_password TEXT NOT NULL
 );
