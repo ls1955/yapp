364e2cec8b44768a3f27ffeb93433480a65f6b1d
Cheong <xxlsxx1955@gmail.com>
2024-04-16 08:56:45 +0800

    Include `user.create` route
    This commit also:
    * Include `templates/user/create.html` form.
    * Include `user.create` route in `base.html`.

--- yapp/user.py
+++ yapp/user.py
@@ -14,3 +14,26 @@ def index():
 
     return render_template("user/index.html", users=users)
 
+
+@bp.route("/create", methods=("GET", "POST"))
+def create():
+    if request.method == "POST":
+        name = request.form["name"]
+        username = request.form["username"]
+        password = request.form["password"]
+        password_confirmation = request.form["password_confirmation"]
+
+        if password != password_confirmation:
+            flash("Password should be equal to password confirmation", "error")
+        else:
+            # TODO: Encrypt password according to encrypt option
+            db = get_db()
+            db.execute(
+                "INSERT INTO users (name, username, password, encrypted_password)"
+                "VALUES (?, ?, ?, ?)",
+                (name, username, password, password)
+            )
+            db.commit()
+            return redirect(url_for("index"))
+    return render_template("user/create.html")
+
