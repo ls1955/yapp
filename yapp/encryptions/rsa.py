a4dec584b77aeebd8c3e8c76a457770f6128568e
Cheong <xxlsxx1955@gmail.com>
2024-04-16 15:03:58 +0800

    Fix wrong method name

--- yapp/encryptions/rsa.py
+++ yapp/encryptions/rsa.py
@@ -2,14 +2,14 @@ import binascii
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-def aes_encrypt(plaintext):
+def rsa_encrypt(plaintext):
     with open("../.keys/public-rsa-key.pem", "rb") as f:
         public_key = RSA.import_key(f.read())
     encryptor = PKCS1_OAEP.new(public_key)
     return encryptor.encrypt(plaintext.encode())
 
 
-def aes_decrypt(ciphertext):
+def rsa_decrypt(ciphertext):
     with open("../.keys/private-rsa-key.pem", "rb") as f:
         private_key = RSA.import_key(f.read())
     decryptor = PKCS1_OAEP.new(private_key)
@@ -18,6 +18,7 @@ def aes_decrypt(ciphertext):
 
 if __name__ == "__main__":
     plaintext = "Goodbye, world."
-    ciphertext = aes_encrypt(plaintext)
+    ciphertext = rsa_encrypt(plaintext)
 
-    assert plaintext == aes_decrypt(ciphertext)
+    assert plaintext == rsa_decrypt(ciphertext)
+
