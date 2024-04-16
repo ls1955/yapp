c908642696e953a58a639569a6fa9bd118aa9717
Cheong <xxlsxx1955@gmail.com>
2024-04-16 14:52:43 +0800

    Extract scripts into functions
    This commit extract RSA encryption and decryption into respective
    functions.

--- yapp/encryptions/rsa.py
+++ yapp/encryptions/rsa.py
@@ -2,19 +2,22 @@ import binascii
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-with open("../.keys/public-rsa-key.pem", "rb") as f:
-    public_key = RSA.import_key(f.read())
-encryptor = PKCS1_OAEP.new(public_key)
+def aes_encrypt(plaintext):
+    with open("../.keys/public-rsa-key.pem", "rb") as f:
+        public_key = RSA.import_key(f.read())
+    encryptor = PKCS1_OAEP.new(public_key)
+    return encryptor.encrypt(plaintext.encode())
 
-# Encrypt a message
-message = 'Hello World'
-encrypted_msg = encryptor.encrypt(message.encode())
-print("Encrypted:", binascii.hexlify(encrypted_msg))
 
-with open("../.keys/private-rsa-key.pem", "rb") as f:
-    private_key = RSA.import_key(f.read())
+def aes_decrypt(ciphertext):
+    with open("../.keys/private-rsa-key.pem", "rb") as f:
+        private_key = RSA.import_key(f.read())
+    decryptor = PKCS1_OAEP.new(private_key)
+    return decryptor.decrypt(ciphertext).decode()
 
-# Decrypt the message
-decryptor = PKCS1_OAEP.new(private_key)
-decrypted_msg = decryptor.decrypt(encrypted_msg)
-print("Decrypted:", decrypted_msg.decode())
+
+if __name__ == "__main__":
+    plaintext = "Goodbye, world."
+    ciphertext = aes_encrypt(plaintext)
+
+    assert plaintext == aes_decrypt(ciphertext)
