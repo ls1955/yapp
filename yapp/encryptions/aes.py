8b89bcf2a0445c8d8aaa3e50742dd9e628f8fd3b
Cheong <xxlsxx1955@gmail.com>
2024-04-16 15:10:53 +0800

    Extract encrypt and decrypt functions

--- yapp/encryptions/aes.py
+++ yapp/encryptions/aes.py
@@ -4,17 +4,30 @@ from Crypto.Random import get_random_bytes
 
 with open("../.keys/aes-key", "rb") as f:
     key = f.read()
-cipher = AES.new(key, AES.MODE_ECB)
 
-# Encrypt a message
-pad = lambda s: s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
-message = 'Hello World'
-padded_message = pad(message)
-encrypted_msg = cipher.encrypt(padded_message.encode())
-print("Encrypted:", binascii.hexlify(encrypted_msg))
+def pad(string):
+    # QUESTION: What is the purpose of this function?
+    return string + (16 - len(string) % 16) * chr(16 - len(string) % 16)
 
-# Decrypt the message
-decrypted_msg = cipher.decrypt(encrypted_msg).decode()
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
-print("Decrypted:", unpad(decrypted_msg))
+
+def aes_encrypt(plaintext):
+    encrypter = AES.new(key, AES.MODE_ECB)
+    return encrypter.encrypt(pad(plaintext).encode())
+
+
+def unpad(string):
+    # QUESTION: What is the purpose of this function?
+    return string[:-ord(string[len(string) - 1:])]
+
+
+def aes_decrypt(ciphertext):
+    decrypter = AES.new(key, AES.MODE_ECB)
+    return unpad(decrypter.decrypt(ciphertext).decode())
+
+
+if __name__ == "__main__":
+    plaintext = "Goodbye, world."
+    ciphertext = aes_encrypt(plaintext)
+
+    assert plaintext == aes_decrypt(ciphertext)
 
--- yapp/encryptions/des.py
+++ yapp/encryptions/des.py
@@ -4,17 +4,30 @@ from Crypto.Random import get_random_bytes
 
 with open("../.keys/des-key", "rb") as f:
     key = f.read()
-cipher = DES.new(key, DES.MODE_ECB)
 
-# Encrypt a message
-pad = lambda s: s + (8 - len(s) % 8) * chr(8 - len(s) % 8)
-message = 'Hello World'
-padded_message = pad(message)
-encrypted_msg = cipher.encrypt(padded_message.encode())
-print("Encrypted:", binascii.hexlify(encrypted_msg))
+def pad(string):
+    # QUESTION: What is the purpose of this function?
+    return string + (8 - len(string) % 8) * chr(8 - len(string) % 8)
 
-# Decrypt the message
-decrypted_msg = cipher.decrypt(encrypted_msg).decode()
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
-print("Decrypted:", unpad(decrypted_msg))
+
+def des_encrypt(plaintext):
+    encrypter = DES.new(key, DES.MODE_ECB)
+    return encrypter.encrypt(pad(plaintext).encode())
+
+
+def unpad(string):
+    # QUESTION: What is the purpose of this function?
+    return string[:-ord(string[len(string) - 1:])]
+
+
+def des_decrypt(ciphertext):
+    decrypter = DES.new(key, DES.MODE_ECB)
+    return unpad(decrypter.decrypt(ciphertext).decode())
+
+
+if __name__ == "__main__":
+    plaintext = "Goodbye, world."
+    ciphertext = des_encrypt(plaintext)
+
+    assert plaintext == des_decrypt(ciphertext)
 
