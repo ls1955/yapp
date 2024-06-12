b2c020943af11e37f05c399d4a5a70e02b0aac3f
Cheong <xxlsxx1955@gmail.com>
2024-06-12 10:56:18 +0800

    Fix import paths
    Something wrong when importing this file from other file.

--- yapp/performance.py
+++ yapp/performance.py
@@ -2,9 +2,9 @@ import os
 import os.path as path
 import time
 
-from encryptions.aes import aes_encrypt
-from encryptions.des import des_encrypt
-from encryptions.rsa import rsa_encrypt
+from yapp.encryptions.aes import aes_encrypt
+from yapp.encryptions.des import des_encrypt
+from yapp.encryptions.rsa import rsa_encrypt
 
 
 def record_performance(encrypt_function):
@@ -27,12 +27,15 @@ base_path = path.dirname(__file__)
 
 
 def write_time_to_file(func, runtime):
+    print(func, runtime)
     with open(dest_file(func), "a") as f:
         # round runtime to 5 decimal place
         f.write(f"{runtime:.5f}\n")
 
 
 def dest_file(func):
+    if isinstance(func, str):
+        return path.abspath(path.join(base_path, "data", "perf", f"{func.lower()}_encrypt.txt"))
     return path.abspath(path.join(base_path, "data", "perf", f"{func.__name__}.txt"))
 
 
