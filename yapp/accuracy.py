b7f539efa61c8c330f46bf8c44037832371a2f2e
Cheong <xxlsxx1955@gmail.com>
2024-06-16 16:38:24 +0800

    Remove records automation

--- yapp/accuracy.py
+++ yapp/accuracy.py
@@ -12,22 +12,6 @@ except ModuleNotFoundError:
     from yapp.encryptions.rsa import rsa_encrypt, rsa_decrypt
 
 
-def record_accuracy(encrypt_func, decrypt_func):
-    """
-    Records the output (Success or Fail) after decrypting password.
-    It will append the recorded time into `accuracy/<encryption_function_name>.txt`.
-    """
-    def wrapper(plaintext):
-        encrypted = encrypt_func(plaintext).decode("latin-1")
-        result = str(decrypt_func(encrypted.encode("latin-1")))
-
-        is_success = result == plaintext
-        write_outcome_to_file(decrypt_func, is_success)
-
-        return result
-    return wrapper
-
-
 base_path = path.dirname(__file__)
 
 
@@ -43,31 +27,3 @@ def dest_file(func):
     if isinstance(func, str):
         return path.abspath(path.join(base_path, "data", "accuracy", f"{func}_decrypt.txt"))
     return path.abspath(path.join(base_path, "data", "accuracy", f"{func.__name__}.txt"))
-
-
-if __name__ == "__main__":
-    usernames_file = path.abspath(path.join(base_path, "data", "usernames.txt"))
-    with open(usernames_file, "r") as f:
-        usernames = f.readlines()
-    assert usernames, "usernames.txt is empty."
-
-    passwords_file = path.abspath(path.join(base_path, "data", "passwords.txt"))
-    with open(passwords_file, "r") as f:
-        passwords = f.readlines()
-    assert passwords, "passwords.txt is empty."
-
-    assert len(usernames) == len(passwords), "usernames and passwords should have same length."
-
-    for encrypt_func, decrypt_func in [(aes_encrypt, aes_decrypt), (des_encrypt, des_decrypt), (rsa_encrypt, rsa_decrypt)]:
-        try:
-            os.makedirs(path.abspath(path.join(base_path, "data", "accuracy")))
-        except OSError:
-            pass
-        # clear previous recorded performance
-        try:
-            os.remove(dest_file(decrypt_func))
-        except FileNotFoundError:
-            pass
-
-        for password in passwords:
-            record_accuracy(encrypt_func, decrypt_func)(password)
--- yapp/performance.py
+++ yapp/performance.py
@@ -11,23 +11,6 @@ except ModuleNotFoundError:
     from yapp.encryptions.des import des_encrypt
     from yapp.encryptions.rsa import rsa_encrypt
 
-
-def record_performance(encrypt_function):
-    """
-    Records the time taken (in seconds) taken to complete the function.
-    It will append the recorded time into `perf/<encryption_function_name>.txt`.
-    """
-    def wrapper(plaintext):
-        init_time = time.time()
-        result = encrypt_function(plaintext)
-        runtime = time.time() - init_time
-
-        write_time_to_file(encrypt_function, runtime)
-
-        return result
-    return wrapper
-
-
 base_path = path.dirname(__file__)
 
 
@@ -42,31 +25,3 @@ def dest_file(func):
     if isinstance(func, str):
         return path.abspath(path.join(base_path, "data", "perf", f"{func.lower()}_encrypt.txt"))
     return path.abspath(path.join(base_path, "data", "perf", f"{func.__name__}.txt"))
-
-
-if __name__ == "__main__":
-    usernames_file = path.abspath(path.join(base_path, "data", "usernames.txt"))
-    with open(usernames_file, "r") as f:
-        usernames = f.readlines()
-    assert usernames, "usernames.txt is empty."
-
-    passwords_file = path.abspath(path.join(base_path, "data", "passwords.txt"))
-    with open(passwords_file, "r") as f:
-        passwords = f.readlines()
-    assert passwords, "passwords.txt is empty."
-
-    assert len(usernames) == len(passwords), "usernames and passwords should have same length."
-
-    for func in [aes_encrypt, des_encrypt, rsa_encrypt]:
-        try:
-            os.makedirs(path.abspath(path.join(base_path, "data", "perf")))
-        except OSError:
-            pass
-        # clear previous recorded performance
-        try:
-            os.remove(dest_file(func))
-        except FileNotFoundError:
-            pass
-
-        for password in passwords:
-            record_performance(func)(password)
